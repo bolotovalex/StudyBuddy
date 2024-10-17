2e3e28df06820f61ee768de27aac583829c4333f
ilexx <stealthfear@gmail.com>
2024-10-17 14:52:20 +0500

    Переименование WinForm проекта

--- WinFormsApp1/Properties/Resources.Designer.cs
+++ WinFormsApp1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsDesktopApp.Properties {
+namespace PryamolineynostWF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormsDesktopApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsDesktopApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PryamolineynostWF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
