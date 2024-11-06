9c9753eaca0345e87260601b2a40c578f48b8d7f
ilexx <stealthfear@gmail.com>
2024-11-06 10:17:57 +0500

    Добавление функции цветового акцента

--- PryamolineysnostModerUI/MainWindow.axaml.cs
+++ PryamolineysnostModerUI/MainWindow.axaml.cs
@@ -1,21 +1,66 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace PryamolineysnostModerUI
 {
     public partial class MainWindow : Window
     {
+        public enum Panels
+        {
+            Home,
+            Data,
+            Graphic,
+            PdfPanel
+        }
+
+        public Panels SelectedPanel = Panels.Home;
+        
         public int Count = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
+            ChangePanel(Panels.Home);
         }
 
-        public void ExitButton_Click()
+
+        private void CheckButton(Button pButton)
         {
-
+            pButton.Background = new SolidColorBrush(Colors.LightSkyBlue);
         }
+
+        private void UncheckButton(Button pButton)
+        {
+            pButton.Background = new SolidColorBrush(Color.FromRgb(247,247,247));
+        }
+
+        private Button GetAccentButton(Panels p)
+        {
+            switch (p)
+            {
+                case Panels.Home:
+                    return HomeButton;
+                case Panels.Data:
+                    return DataButton;
+                case Panels.Graphic:
+                    return GraphicButton;
+                case Panels.PdfPanel:
+                    return PdfButton;
+            }
+
+            return HomeButton;
+        }
+        private void ChangePanel(Panels panel)
+        {
+            var prevButton = GetAccentButton(SelectedPanel);
+            prevButton.Background = new SolidColorBrush(Color.FromRgb(247,247,247));
+            SelectedPanel = panel;
+            var nextButton = GetAccentButton(SelectedPanel);
+            nextButton.Background = new SolidColorBrush(Colors.LightSkyBlue);
+        }
+       
 
         private void ExitButton_OnClick(object? sender, RoutedEventArgs e)
         {
