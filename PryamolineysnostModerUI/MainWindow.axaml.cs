db12621acc5640f6f7128caee98be7c5c81cb974
Alexey Bolotov <stealthfear@gmail.com>
2024-11-10 23:12:36 +0500

    Доработка интерфейса к единой концепции.

--- PryamolineysnostModerUI/MainWindow.axaml.cs
+++ PryamolineysnostModerUI/MainWindow.axaml.cs
@@ -7,30 +7,48 @@ namespace PryamolineysnostModerUI
 {
     public partial class MainWindow : Window
     {
-        public enum Panels
+        public enum PressedButton
         {
             Home,
             Data,
             Graphic,
-            PdfPanel
+            Pdf,
+            Load,
+            Save,
+            Settings,
+            Exit
         }
 
-        public Panels SelectedPanel = Panels.Home;
+        private PressedButton SelectedPressedButton = PressedButton.Home;
+        private PressedButton PrevPressedButton = PressedButton.Exit;
         
-        public int Count = 0;
+        private int Count = 0;
         
         public MainWindow()
         {
             InitializeComponent();
-            ChangePanel(Panels.Home);
+            ChangePanel(PressedButton.Home);
             DataPanel.IsVisible = false;
             GraphicPanel.IsVisible = false;
+            PdfPanel.IsVisible = false;
+            SavePanel.IsVisible = false;
+            LoadPanel.IsVisible = false;
+            SettingsPanel.IsVisible = false;
+            ExitPanel.IsVisible = false;
         }
 
 
         private void CheckButton(Button pButton)
         {
-            pButton.Background = new SolidColorBrush(Colors.LightSkyBlue);
+            if (SelectedPressedButton == PressedButton.Exit)
+            {
+                pButton.Background = new SolidColorBrush(Colors.LightCoral);    
+            }
+            else
+            {
+                pButton.Background = new SolidColorBrush(Colors.LightSkyBlue);
+            }
+            
         }
 
         private void UncheckButton(Button pButton)
@@ -38,71 +56,124 @@ namespace PryamolineysnostModerUI
             pButton.Background = new SolidColorBrush(Color.FromRgb(247,247,247));
         }
 
-        private Button GetAccentButton(Panels p)
+        private Button GetAccentButton(PressedButton p)
         {
             switch (p)
             {
-                case Panels.Home:
+                case PressedButton.Home:
                     return HomeButton;
-                case Panels.Data:
+                case PressedButton.Data:
                     return DataButton;
-                case Panels.Graphic:
+                case PressedButton.Graphic:
                     return GraphicButton;
-                case Panels.PdfPanel:
+                case PressedButton.Pdf:
                     return PdfButton;
+                case PressedButton.Save:
+                    return SaveButton;
+                case PressedButton.Load:
+                    return LoadButton;
+                case PressedButton.Settings:
+                    return SettingsButton;
+                case PressedButton.Exit:
+                    return ExitButton;
             }
 
             return HomeButton;
         }
 
-        private Panel GetPanel(Panels p)
+        private Panel GetPanel(PressedButton p)
         {
             switch (p)
             {
-                case Panels.Home:
+                case PressedButton.Home:
                     return MainPanel;
-                case Panels.Data:
+                case PressedButton.Data:
                     return DataPanel;
-                case Panels.Graphic:
-                    return GraphicPanel;    
+                case PressedButton.Graphic:
+                    return GraphicPanel;  
+                case PressedButton.Pdf:
+                    return PdfPanel;
+                case PressedButton.Load:
+                    return LoadPanel;
+                case PressedButton.Save:
+                    return SavePanel;
+                case PressedButton.Settings:
+                    return SettingsPanel;
+                case PressedButton.Exit:
+                    return ExitPanel;
             }
 
             return MainPanel;
         }
         
-        private void ChangePanel(Panels panel)
+        private void ChangePanel(PressedButton pressedButton)
         {
-            var prevButton = GetAccentButton(SelectedPanel);
+            PrevPressedButton = SelectedPressedButton;
+            
+            var prevButton = GetAccentButton(SelectedPressedButton);
             UncheckButton(prevButton);
-            var prevPanel = GetPanel(SelectedPanel);
+            var prevPanel = GetPanel(SelectedPressedButton);
             prevPanel.IsVisible = false;
-            SelectedPanel = panel;
-            var nextButton = GetAccentButton(SelectedPanel);
+            
+            SelectedPressedButton = pressedButton;
+            
+            var nextButton = GetAccentButton(SelectedPressedButton);
             CheckButton(nextButton);
-            var nextPanel = GetPanel(SelectedPanel);
+            var nextPanel = GetPanel(SelectedPressedButton);
             nextPanel.IsVisible = true;
         }
        
 
-        private void ExitButton_OnClick(object? sender, RoutedEventArgs e)
+        private void ConfirmExitButton_OnClick(object? sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        
+        private void UnconfirmExitButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PrevPressedButton);
         }
 
         private void HomeButton_Click(object? sender, RoutedEventArgs e)
         {
-            ChangePanel(Panels.Home);
+            ChangePanel(PressedButton.Home);
         }
 
         private void DataButton_Click(object? sender, RoutedEventArgs e)
         {
-            ChangePanel(Panels.Data);
+            ChangePanel(PressedButton.Data);
         }
 
         private void GraphicButton_Click(object? sender, RoutedEventArgs e)
         {
-            ChangePanel(Panels.Graphic);
+            ChangePanel(PressedButton.Graphic);
         }
+        
+        private void PdfButton_Click(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PressedButton.Pdf);
+        }
+        
+        private void LoadButton_Click(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PressedButton.Load);
+        }
+        
+        private void SaveButton_Click(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PressedButton.Save);
+        }
+        
+        private void SettingsButton_Click(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PressedButton.Settings);
+        }
+        
+        private void ExitButton_Click(object? sender, RoutedEventArgs e)
+        {
+            ChangePanel(PressedButton.Exit);
+        }
+        
 
         private void Counter_Click(object? sender, RoutedEventArgs e)
         {
