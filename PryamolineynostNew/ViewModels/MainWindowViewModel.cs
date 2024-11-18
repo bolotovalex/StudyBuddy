8ef73a9966bbea378b3efdb207644df20af0aff7
ilexx <stealthfear@gmail.com>
2024-11-18 15:44:34 +0500

    New interface INIT

---
+++ PryamolineynostNew/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Mime;
+using System.Xml;
+using Avalonia;
+using PryamolineynostNew.Views;
+using Tmds.DBus.Protocol;
+
+namespace PryamolineynostNew.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+#pragma warning disable CA1822 // Mark members as static
+        private string _asd;
+        public string Greeting => "Welcome to Avalonia!";
+        public void TextChange()
+        {
+        }
+        
+        public string NameAsd
+        {
+            get => _asd;
+            set => SetProperty(ref _asd, value);
+        }
+
+        public void ExitButton_Click()
+        {
+            Environment.Exit(0);
+        }
+#pragma warning restore CA1822 // Mark members as static
+    }
+}
---
+++ PryamolineynostNew/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PryamolineynostNew.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
