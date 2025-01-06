6841204e6112ac63aae3d60fa5c40e87a69d8b43
Alexey Bolotov <stealthfear@gmail.com>
2025-01-06 13:53:59 +0500

    Добавлена модель для документа

---
+++ StudyBuddy/documents/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from groups.models import StudyGroup
+
+class Document(models.Model):
+    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE, related_name='documents')
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='group_documents/')
+    description = models.TextField(blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
--- StudyBuddy/meetings/models.py
+++ StudyBuddy/meetings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from groups.models import StudyGroup
 
-# Create your models here.
+class VideoMeeting(models.Model):
+    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE, related_name='meetings')
+    title = models.CharField(max_length=255, verbose_name="Название встречи")
+    link = models.URLField(verbose_name="Ссылка на встречу")
+    #TODO Нужно добавить выбор даты
+    scheduled_at = models.DateTimeField(verbose_name="Дата и время встречи")
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
