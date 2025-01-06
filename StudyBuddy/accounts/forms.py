752726e791636a66eed609aa451703b79c7e917b
Alexey Bolotov <stealthfear@gmail.com>
2025-01-06 20:10:26 +0500

    Добавлена проверка уникальности логина и почты

--- StudyBuddy/accounts/forms.py
+++ StudyBuddy/accounts/forms.py
@@ -1,13 +1,37 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
 
 from .models import User, Profile
 
 
-class CustomUserCreationForm(UserCreationForm):
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'role')
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Пользователь с таким именем уже существует.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с таким email уже существует.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError("Пароли не совпадают.")
+        return cleaned_data
 
 
 class UserLoginForm(AuthenticationForm):
