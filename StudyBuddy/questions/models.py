db1502af762501fd605fa6f40836be5b2eed87f2
Alexey Bolotov <stealthfear@gmail.com>
2025-01-08 14:26:36 +0500

    Добавлены коментарии questions

--- StudyBuddy/questions/models.py
+++ StudyBuddy/questions/models.py
@@ -3,18 +3,33 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 class Question(models.Model):
+    """
+    Модель для вопроса.
+    """
+    # Текст вопроса
     question_text = models.TextField()
+    # Пользователь, создавший вопрос
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions')
+    # Дата и время создания вопроса
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        # Возвращает первые 50 символов текста вопроса
         return self.question_text[:50] + ('...' if len(self.question_text) > 50 else '')
 
 class Answer(models.Model):
+    """
+    Модель для ответа на вопрос.
+    """
+    # Связь с вопросом
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    # Текст ответа
     answer_text = models.TextField()
+    # Пользователь, создавший ответ
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='answers')
+    # Дата и время создания ответа
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        # Возвращает первые 50 символов текста ответа
         return self.answer_text[:50] + ('...' if len(self.answer_text) > 50 else '')
--- StudyBuddy/questions/views.py
+++ StudyBuddy/questions/views.py
@@ -4,12 +4,18 @@ from .models import Question, Answer
 
 @login_required
 def question_list(request):
+    """
+    Отображает список вопросов. Если задан параметр поиска, фильтрует вопросы по тексту.
+    """
     query = request.GET.get('q', '')
     questions = Question.objects.filter(question_text__icontains=query) if query else Question.objects.all()
     return render(request, 'questions/question_list.html', {'questions': questions, 'query': query})
 
 @login_required
 def create_question(request):
+    """
+    Создаёт новый вопрос.
+    """
     if request.method == 'POST':
         question_text = request.POST.get('question_text')
         Question.objects.create(question_text=question_text, created_by=request.user)
@@ -18,6 +24,9 @@ def create_question(request):
 
 @login_required
 def question_detail(request, pk):
+    """
+    Отображает детали вопроса и список ответов на него. Позволяет добавить новый ответ.
+    """
     question = get_object_or_404(Question, pk=pk)
     answers = question.answers.all()
     if request.method == 'POST':
@@ -28,8 +37,12 @@ def question_detail(request, pk):
         'question': question,
         'answers': answers,
     })
+
 @login_required
 def delete_question(request, pk):
+    """
+    Удаляет вопрос, если текущий пользователь является его создателем.
+    """
     question = get_object_or_404(Question, pk=pk, created_by=request.user)
     question.delete()
     return redirect('questions:question_list')
