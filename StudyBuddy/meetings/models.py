7f447ba7cbe71644b210dbc044a905c3ebebbb18
Alexey Bolotov <stealthfear@gmail.com>
2025-01-08 14:22:56 +0500

    Обновлены коментарии meetings

--- StudyBuddy/meetings/models.py
+++ StudyBuddy/meetings/models.py
@@ -1,25 +1,34 @@
-from django.conf import settings
 from groups.models import StudyGroup
 from django.db import models
 from django.contrib.auth.models import User
-from groups.models import StudyGroup
-import uuid
-
 from django.conf import settings
 import uuid
 
 class VideoMeeting(models.Model):
+    """
+    Модель для видеоконференции.
+    """
+    # Связь с учебной группой
     group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE, related_name='meetings')
+    # Название встречи
     title = models.CharField(max_length=255)
+    # Ссылка на видеоконференцию
     link = models.URLField(blank=True, null=True)
+    # Запланированное время встречи
     scheduled_at = models.DateTimeField()
+    # Пользователь, создавший встречу
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
+        """
+        Действие при нажатии на кнопку сохранить при создании встречи.
+        Формируется произвольная ссылка для сервера JITSI, если ссылка не указана.
+        """
         if not self.link:
             unique_meeting_name = f"{uuid.uuid4().hex[:8]}-{uuid.uuid4().hex[:4]}"
             self.link = f"{settings.JITSI_BASE_URL}/{unique_meeting_name}"
         super().save(*args, **kwargs)
 
     def __str__(self):
+        # Возвращает название встречи
         return self.title
--- StudyBuddy/meetings/views.py
+++ StudyBuddy/meetings/views.py
@@ -5,6 +5,9 @@ from .forms import MeetingForm
 
 @login_required
 def add_meeting_view(request, group_id):
+    """
+    Добавляет новое собрание для указанной группы.
+    """
     group = get_object_or_404(StudyGroup, pk=group_id)
     if request.method == 'POST':
         form = MeetingForm(request.POST)
@@ -13,7 +16,9 @@ def add_meeting_view(request, group_id):
             meeting.group = group
             meeting.created_by = request.user
             meeting.save()
+            # Перенаправляем на страницу группы после добавления собрания
             return redirect('groups:group_detail', pk=group.id)
     else:
         form = MeetingForm()
+    # Отображаем форму добавления собрания
     return render(request, 'add_meeting.html', {'form': form, 'group': group})
