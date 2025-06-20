0a908267b1967b883a7873552ae4fcb1e949d1f5
Alexey Bolotov <stealthfear@gmail.com>
2025-06-20 09:57:00 +0500

    Исправление путей для PWA

--- StudyBuddy/static/serviceworker.js
+++ StudyBuddy/static/serviceworker.js
@@ -1,20 +1,23 @@
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('studybuddy-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/staticfiles/icons/icon-192x192.png',
-          '/staticfiles/icons/icon-512x512.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+  e.waitUntil(
+    caches.open('studybuddy-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/static/icon-192x192.png',
+        '/static/icon-512x512.png',
+        '/static/manifest.json',
+        '/static/favicon.ico'
+      ]);
+    }).catch(function(err) {
+      console.error('Ошибка кэширования:', err);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
